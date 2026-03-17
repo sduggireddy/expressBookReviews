7fa29468e4d05ff47e0da232d9d0c97bd01d9580
sduggireddy <dsridhar10@gmail.com>
2026-03-17 13:58:19 -0400

    task 1, 2 and 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -13,11 +12,19 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;    
+    if (books[isbn]) {
+        return res.status(200).send(JSON.stringify(books[isbn], null, 4));      
+    } else {
+        return res.status(200).send(`cannot find book with isbn ${isbn}`);      
+    }
+
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
@@ -25,7 +32,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;  
+  author = author.toLowerCase();  
+  if (author) {
+    const booksArray = Object.values(books);
+    const filtered_books = booksArray.filter((book) => book.author.toLowerCase() === author);
+    if (filtered_books.length) {
+        return res.status(200).send(JSON.stringify(filtered_books, null, 4));
+    } else {
+        return res.status(200).send(`Unable to find books with author ${author}`);
+    }    
+  } else {
+    return res.status(200).send("Author not specified");
+  }  
 });
 
 // Get all books based on title
