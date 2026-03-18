76fb6f2289972bc40eca72c639e7451b76937b47
Sridhar Duggireddy <dsridhar10@gmail.com>
2026-03-18 07:40:53 -0400

    Implement user registration and book retrieval features

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,33 +5,40 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+  // get username and password
   const username = req.body.username;
   const password = req.body.password;
   
   if (username && password) {
     if (!isValid(username)) {
+        // register the user by adding to users array
         users.push({"username": username, "password": password});
         return res.status(200).json({ message: "User successfully registered. Now you can login" });
     } else {
+        // send error message
         return res.status(404).json({ message: "User already exists" });
     }    
   }
+  // send error message
   return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   try {
+    // get books using promise
     const bookList = await new Promise((resolve, reject) => {
         try {
             resolve(books);
         } catch (err) {
+            // send error message
             reject(err);
         }
     });
+    // return book list
     res.send(JSON.stringify(bookList, null, 4));
   } catch(err) {
+    // send error message
     res.status(500).json({ message: "Error retrieving books" });
   }  
 });
@@ -40,15 +47,19 @@ public_users.get('/', async function (req, res) {
 public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
     try {
+        // get bookdetails using promise
         const book = await new Promise((resolve, reject) => {
             try {
                 resolve(books[isbn]);
             } catch (err) {
+              // send error message
                 reject(err);
             }
         });
+      // send book details
         return res.status(200).send(JSON.stringify(book, null, 4));      
     } catch(err) {
+      // send error message
         res.status(500).json({ message: "Error retrieving book" });
     }    
  });
@@ -58,46 +69,59 @@ public_users.get('/author/:author', async function (req, res) {
   let author = req.params.author;  
   author = author.toLowerCase(); 
   try {
+    // get bookdetails using promise
     const filtered_books = await new Promise((resolve, reject) => {
         try {
+            // convert to array to filter easily
             const booksArray = Object.values(books);
             resolve(booksArray.filter((book) => book.author.toLowerCase() === author));
         } catch (err) {
+          // send error message
             reject(err);
         }
-    })
+    });
+    // send book details
     return res.status(200).send(JSON.stringify(filtered_books, null, 4));
   } catch (err) {
+    // send error message
     res.status(500).json({ message: "Error retrieving book" });
   }  
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
-  //Write your code here
+  // fetch title
   let title = req.params.title;  
   title = title.toLowerCase();  
   try {
+    // get bookdetails using promise
     const filtered_books = await new Promise((resolve, reject) => {
         try {
+          // convert to array to filter easily
             const booksArray = Object.values(books);
             resolve(booksArray.filter((book) => book.title.toLowerCase() === title));
         } catch (err) {
+          // send error message
             reject(err);
         }
-    })
+    });
+    // send book details
     return res.status(200).send(JSON.stringify(filtered_books, null, 4));
   } catch (err) {
+    // send error message
     res.status(500).json({ message: "Error retrieving book" });
   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+    // fetch isbn number
     const isbn = req.params.isbn;    
     if (books[isbn]) {
+        // return reviews if the book exists
         return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));      
     } else {
+        // send error message
         return res.status(200).send(`cannot find book with isbn ${isbn}`);      
     }
 
